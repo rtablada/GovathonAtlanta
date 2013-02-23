5ba10b350b7938ec68b53019f5e2a8c56d6ac219
Ryan Tablada <ryan.tablada@gmail.com>
2013-02-23 10:34:16 -0500

    Database to sere1.com

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => 'sere1.com',
 			'database'  => 'govathon',
 			'username'  => 'root',
-			'password'  => 'root',
+			'password'  => 'mysql23',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
